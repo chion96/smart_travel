0b81673c9a972c016a7abf90d65a11aee5669880
ChiOn <chion1996@gmail.com>
2018-11-03 17:15:13 +0800

    Added first route to SmartTravel index.

--- index.js
+++ index.js
@@ -4,7 +4,18 @@
 
 if (process.env.NODE_ENV === 'production') {
   process.env.webpackAssets = JSON.stringify(require('./dist/client/manifest.json'));
-  process.env.webpackChunkAssets = JSON.stringify(require('./dist/client/chunk-manifest.json'));
+  try {
+    process.env.webpackChunkAssets = JSON.stringify(require('./dist/client/chunk-manifest.json'));
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log(`Error reading ${file}:\n`, err.message);
+      console.log('If this is for chunk-manifest.json, it probably means no chunking was used to split app into several bundles.')
+      console.log('Defaulting to empty {} for manifest.')
+    } else {
+      throw err;
+    }
+  }
+  
   // In production, serve the webpacked server file.
   require('./dist/server.bundle.js');
 } else {
